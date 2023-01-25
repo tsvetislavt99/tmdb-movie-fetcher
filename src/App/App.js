ee6177938ffb1c1b012339e23bf4c24d6b2387ec
Tsvetislav <tsvetislav.todorov@mentormate.com>
2023-01-25 15:24:33 +0200

    Add routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,17 @@
-import LandingLayout from 'Pages/Landing';
+import { Routes, Route } from 'react-router-dom';
+
+import FilterPage from 'Pages/FilterPage';
+import LandingPage from 'Pages/LandingPage';
+import ResultsPage from 'Pages/ResultsPage';
 
 function App() {
   return (
     <div className="flex justify-center mt-10 lg:mt-0 lg:items-center min-h-screen">
-      <LandingLayout />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/filter" element={<FilterPage />} />
+        <Route path="/results" element={<ResultsPage />} />
+      </Routes>
     </div>
   );
 }
